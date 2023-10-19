0620cbb540e225b39b385513b8f252cb3aa947f2
pan-ti <pandeandromeda@gmail.com>
2023-10-18 22:08:32 -0300

    Estados para armazenar tarefas, tarefas concluídas, texto da nova tarefa, filtro, termo de busca, ID de tarefa em edição e texto editado.

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,11 +1,13 @@
-import S from "./container.css"
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import S from '../Container/container.css';
 
 const Container = () => {
-  return  (<container className= {S.container}>
-    
-    </container>
-    )
-};
-
-
-export default Container;
+  // Estados para armazenar tarefas, tarefas concluídas, texto da nova tarefa, filtro, termo de busca, ID de tarefa em edição e texto editado.
+  const [tarefas, setTarefas] = useState([]);
+  const [tarefasConcluidas, setTarefasConcluidas] = useState([]);
+  const [novaTarefa, setNovaTarefa] = useState(''); // Texto da nova tarefa
+  const [filtro, setFiltro] = useState('all'); // Filtro (all, active, completed)
+  const [termoBusca, setTermoBusca] = useState(''); // Termo de busca para filtrar tarefas
+  const [idTarefaEditando, setIdTarefaEditando] = useState(null); // ID da tarefa em edição
+  const [textoTarefaEditada, setTextoTarefaEditada] = useState(''); // Texto editado da tarefa
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,19 +1,17 @@
-import Container from '../components/Container/Container'
+import Container from "../components/Container/Container";
 import "../pages/globalStyles.css";
-import Header from '../components/Header/Header';
-
+import Header from "../components/Header/Header";
 
 function App() {
-
   return (
-    <div className='header'>
-      <Header/>
+    <div className="header">
+      <Header />
 
-    <div className='container'> 
-<Container/>
+      <div className="container">
+        <Container />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
