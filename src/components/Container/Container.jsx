296679d660655ebe4577b1ce04c284e5b2230a70
pan-ti <pandeandromeda@gmail.com>
2023-10-18 22:43:26 -0300

    criado função de adiconar uma tarefa e para marcar como concluída

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -11,3 +11,22 @@ const Container = () => {
   const [termoBusca, setTermoBusca] = useState(''); // Termo de busca para filtrar tarefas
   const [idTarefaEditando, setIdTarefaEditando] = useState(null); // ID da tarefa em edição
   const [textoTarefaEditada, setTextoTarefaEditada] = useState(''); // Texto editado da tarefa
+
+  // Função para adicionar uma nova tarefa
+  const adicionarTarefa = () => {
+    if (novaTarefa.trim() !== '') {
+      const novaTarefaObj = { id: uuidv4(), texto: novaTarefa, concluida: false };
+      setTarefas([...tarefas, novaTarefaObj]); // Adiciona nova tarefa ao estado tarefas
+      setNovaTarefa(''); // Limpa o campo de entrada
+    }
+  };
+// Função para marcar uma tarefa como concluída
+const marcarTarefaConcluida = (id) => {
+  const tarefaParaConcluir = tarefas.find(tarefa => tarefa.id === id);
+  if (tarefaParaConcluir) {
+    setTarefasConcluidas([...tarefasConcluidas, tarefaParaConcluir]); // Move tarefa concluída para tarefasConcluidas
+    setTarefas(tarefas.filter(tarefa => tarefa.id !== id)); // Remove tarefa concluída de tarefas
+  }
+};
+
+  export default Container;
